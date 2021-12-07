7e5435b805a6ecc526209114cc49aaec67385482
Stas <stas.sklyar.dev@gmail.com>
2021-12-07 10:39:36 +0300

    in progress

--- _stas_front/bootstrap5/category/category.js
+++ _stas_front/bootstrap5/category/category.js
@@ -2,10 +2,41 @@ $(function() {
     $('.js_selectpicker').selectpicker();
 });
 
+////////////////////////////////////////////////////
+
 const filterAdaptiveMenu = document.querySelector("#category__filter-adaptive-menu")
 const filterAdaptiveMenuBtn = document.querySelector("#category__filter-adaptive-menu-btn")
 
 filterAdaptiveMenuBtn.addEventListener(('click'), (e) => {
-    console.log(e)
     filterAdaptiveMenu.classList.toggle("category__filter-adaptive-menu--active")
+})
+
+///////////////////////////////////////////////////
+
+const categoryCardBtns = document.querySelectorAll(".category-card__btn")
+
+categoryCardBtns.forEach((btn) => {
+    btn.addEventListener(('click'), (e) => {
+        btn.classList.toggle("category-card__btn--active")
+    })
+})
+
+////////////////////////////////////////////////////
+
+const filterBoxBtns = document.querySelectorAll(".filter-box__btn")
+const resetFilterBtns = document.querySelectorAll(".filter-box__reset-btn")
+
+filterBoxBtns.forEach((btn) => {
+    btn.addEventListener(('click'), (e) => {
+        btn.classList.toggle("filter-box__btn--active")
+    })
+})
+
+resetFilterBtns.forEach((btn) => {
+    console.log("foreach")
+    btn.addEventListener(('click'), (e) => {
+        filterBoxBtns.forEach((btn) => {
+            btn.classList.remove("filter-box__btn--active")
+        })
+    })
 })
